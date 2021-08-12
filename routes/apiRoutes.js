36ed51f055ee6f4772d1a07b1e3580b54cbb806b
Ted Keim <tedkeim@yahoo.com>
2021-08-12 06:52:43 -0400

    had to fix errors on api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,12 @@
 const router = require('express').Router(); 
 const db = require('../models');
 
-router.get("/api/workouts", (req, res) => {
-        dbWorkout.forEach(workout => {
-            var total = 0;
-            workout.exercises.forEach(e => {
-                total += e.duration;
-            });
-            workout.totalDuration = total;   
-        });
+router.post('/api/workouts', ({ body }, res) => {
+    db.Workout.create(body)
+    .then(dbWorkout => { res.json(dbWorkout) } )
+    .catch(err => { res.json(err) });
+});
 
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.json(err);
-
-    });
-
-    router.post("/api/workouts", ({ body }, res) => {
-        // console.log("WORKOUT TO BE ADDED");
-        // console.log(body);
-
-        db.Workout.create(body).then((dbWorkout => {
-            res.json(dbWorkout);
-        })).catch(err => {
-            res.json(err);
-        });
-    });
 
     router.get("/api/workouts/range", (req, res) => {
 
