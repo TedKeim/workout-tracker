a68cb62c1504ef5e2dd85374a2de8d2bc764abc6
Ted Keim <tedkeim@yahoo.com>
2021-08-12 06:48:06 -0400

    fixed errors and added models folder with index.js and workout.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const db = require('../models');
 
 router.get("/api/workouts", (req, res) => {
-      console.log("ALL WORKOUTS");
-        console.log(dbWorkout);
         dbWorkout.forEach(workout => {
             var total = 0;
             workout.exercises.forEach(e => {
