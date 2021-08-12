34ee0e8c888b1bc4e42ff1e44237252a99079164
Ted Keim <tedkeim@yahoo.com>
2021-08-12 06:29:10 -0400

    added to apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router(); 
 const db = require('../models');
 
+router.get("/api/workouts", (req, res) => {
+      console.log("ALL WORKOUTS");
+        console.log(dbWorkout);
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+        });
