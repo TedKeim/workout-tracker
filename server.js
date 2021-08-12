afe9e6b90ec184a42744f444dbc39902031d13e7
Ted Keim <tedkeim@yahoo.com>
2021-08-12 07:07:24 -0400

    added to server.js to deploy to heroku

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ mongoose.connect(
     }
   );
 
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
