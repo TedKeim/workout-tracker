d2097b7ab98437d9dd9a699b5713c837d862fb32
Ted Keim <tedkeim@yahoo.com>
2021-08-12 06:33:48 -0400

    added get method on api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -21,3 +21,26 @@ router.get("/api/workouts", (req, res) => {
     router.post("/api/workouts", ({ body }, res) => {
         // console.log("WORKOUT TO BE ADDED");
         // console.log(body);
+
+        db.Workout.create(body).then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+    });
+
+    router.get("/api/workouts/range", (req, res) => {
+
+        db.Workout.find({}).then(dbWorkout => {
+            console.log("ALL WORKOUTS");
+            console.log(dbWorkout);
+    
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+    
+    });
+    
+    
+    module.exports = router;
