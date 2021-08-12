1048105161b4cdc41d2ded84bae6061a3d8686d7
Ted Keim <tedkeim@yahoo.com>
2021-08-12 06:56:27 -0400

    added to api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,19 +7,45 @@ router.post('/api/workouts', ({ body }, res) => {
     .catch(err => { res.json(err) });
 });
 
+router.put('/api/workouts/:id', (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, 
+        { $push: { exercises: req.body } }, 
+        { new: true, runValidators: true }
+    )
+    .then(dbWorkout => { res.json(dbWorkout); })
+    .catch(err => { res.json(err); });
+});
+
 
     router.get("/api/workouts/range", (req, res) => {
-
-        db.Workout.find({}).then(dbWorkout => {
-            console.log("ALL WORKOUTS");
-            console.log(dbWorkout);
+        db.Workout.find({})
+        .then(dbWorkouts => {
+            const workouts = dbWorkouts.map(workout => {
+                const duration = workout.exercises.reduce((acc,next) => {
+                    return acc + next.duration;
+                }, 0);
     
-            res.json(dbWorkout);
-        }).catch(err => {
-            res.json(err);
+                return {
+                    
+                    totalDuration: duration, 
+                    ...workout.toObject()
+                }
+            })
+            res.json(workouts); 
+        })
+        .catch(err => {
+            res.json(err); 
         });
-    
     });
+
+    router.get('/api/workouts', (req, res) => {
+        db.Workout.find({})
+        .then(dbWorkout => { res.json(dbWorkout); })
+        .catch(err => { res.json(err); })
+    });
+
+     
     
     
     module.exports = router;
